812c9f2554835588f96d200e9feb7cb3e78a2d63
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-08-11 23:51:52 -0700

    Delete autostrength.lua

--- Scripts/autostrength.lua
+++
@@ -1,8 +0,0 @@
-while wait() do
-    local args = {
-    [1] = 57,
-    [2] = Vector3.new(-1133.0262451171875, 2.22735595703125, 1022.6928100585938)
-}
-
-game:GetService("Players").LocalPlayer.Character.Combat.LocalScript.Combat:FireServer(unpack(args))
-end
