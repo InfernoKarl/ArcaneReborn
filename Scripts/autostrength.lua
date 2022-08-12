5216487fee557a3f5284e6e05a955ed45f363d26
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-08-11 23:34:07 -0700

    + Auto Strength (working as of 11/8/2022)

---
+++ Scripts/autostrength.lua
@@ -0,0 +1,8 @@
+while wait(0.1) do
+    local args = {
+    [1] = 57,
+    [2] = Vector3.new(-1133.0262451171875, 2.22735595703125, 1022.6928100585938)
+}
+
+game:GetService("Players").LocalPlayer.Character.Combat.LocalScript.Combat:FireServer(unpack(args))
+end
