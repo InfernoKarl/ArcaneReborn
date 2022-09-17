acab74dc7ba4b4d69249e4579c83ddb2529ffb5d
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-09-16 20:23:12 -0700

    this literally works wtf

---
+++ Scripts/infinitemagicenergy.lua
@@ -0,0 +1,13 @@
+getgenv().MagEn = true
+
+local Client = game:GetService("Players").LocalPlayer
+local MagicEnergy = Client.bin.MagicEnergy
+
+OldNewIndex = hookmetamethod(game, "__newindex", function(self, index, value)
+    if MagEn and checkcaller() == false then
+        if (self == MagicEnergy) and index == "Value" then
+            value = 605
+        end
+    end
+    return OldNewIndex(self, index, value)
+end)
